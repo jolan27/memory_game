f364044ed96e3c1a0ba35f778cfd6d96c5872514
jolan27 <163869453+jolan27@users.noreply.github.com>
2024-11-13 08:14:47 +0100

    Début momry game

---
+++ src/Cartes.js
@@ -0,0 +1,24 @@
+export function carte () {
+    const tableCards = []
+    const carte1 = {
+        name : 'salut',
+        etat : 'false'
+    }
+
+    const carte2 = {
+        name : 'salut',
+        etat : 'false'
+    }
+
+    const carte3 = {
+        name : 'yo',
+        etat : 'false'
+    }
+
+    const carte4 = {
+        name : 'yo',
+        etat : 'false'
+    }
+
+    tableCards.push(carte1,carte2, carte3, carte4)
+}
---
+++ src/Grille.js
@@ -0,0 +1,27 @@
+export function Grille () {
+
+    const ligne = 4
+    const colonne = 4
+
+    return (
+
+    )
+
+
+
+
+
+
+
+    // var grille = document.createElement ('table')
+
+    // for (var i=0; i<4; i++) {
+    //     var ligne = document.createElement ('tr')
+    //     grille.appendChild(ligne)
+    // }
+
+    // for (var j=0; j<; j++) {
+    //     var colonne = document.createElement('td')
+    //     ligne.appendChild(colonne)
+    // }
+}
